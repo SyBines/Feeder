c560179e7c2e568d2fa3047095bb2159f1355d27
SyBines <seymourb5@nycstudents.net>
2024-09-19 11:52:24 -0400

    Update Feeder.java

--- Feeder.java
+++ Feeder.java
@@ -4,24 +4,49 @@ public class Feeder {
      * The amount of food, in grams, currently in the bird feeder; initialized
      * in the constructor and always greater than or equal to zero
      */
+    public Feeder(int c)
+    {
+        currentFood = c;
+    }
+    public String toString()
+    {
+        return "Current Food: " + currentFood;
+    }
+
+
+    public Feeder() {}
     private int currentFood;
 
     /**
      * Simulates one day with numBirds birds or possibly a bear at the bird
      * feeder, as described in part (a) Precondition: numBirds > 0
      */
-    public void simulateOneDay(int numBirds)
-    {
-    }
+    public void simulateOneDay(int numBirds) {
 
+        if(Math.random() < 0.05) currentFood = 0;
+        else
+        {
+            int eaten = (int)(Math.random() * 41) + 10;
+            currentFood -= eaten * numBirds;
+            if(currentFood < 0) currentFood = 0;
+        }
+
+    }
     /**
      * Returns the number of days birds or a bear found food to eat at the
      * feeder in this simulation, as described in part (b) Preconditions:
      * numBirds > 0, numDays > 0
      */
-    public int simulateManyDays(int numBirds, int numDays) 
-    {
+    public int simulateManyDays(int numBirds, int numDays) {
         return 0;
     }
 
+    public int getCurrentFood()
+    {
+        return currentFood;
+    }
+
 }
+
+
+
