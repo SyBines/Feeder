fb2761a898e63cbb6d437a0a0f2ac76df355eed0
SyBines <seymourb5@nycstudents.net>
2024-09-19 11:52:07 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Feeder f = new Feeder();
-        System.out.println(f);
+        Feeder g = new Feeder(500);
+        System.out.println(g);
+        //System.out.println(f.currentFood);
+        //System.out.println(g.getCurrentFood());
+        g.simulateOneDay(12);
+        Feeder i = new Feeder(100);
+        i.simulateOneDay(5);
+        System.out.println(i);
     }
 }
