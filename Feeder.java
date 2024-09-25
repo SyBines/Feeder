e6996a41cf3d3f91073550adaec4dbef1c842730
sybines <seymourb5@nycstudents.net>
2024-09-25 11:37:45 -0400

    Tested different commands

--- Feeder.java
+++ Feeder.java
@@ -1,14 +1,11 @@
 public class Feeder {
 
-    /**
-     * The amount of food, in grams, currently in the bird feeder; initialized
-     * in the constructor and always greater than or equal to zero
-     */
+
     public Feeder(int c)
     {
         currentFood = c;
     }
-    public String toString(int numBirds)
+    public String toString()
     {
         return "Current Food: " + currentFood;
     }
@@ -17,10 +14,6 @@ public class Feeder {
     public Feeder() {}
     private int currentFood;
 
-    /**
-     * Simulates one day with numBirds birds or possibly a bear at the bird
-     * feeder, as described in part (a) Precondition: numBirds > 0
-     */
     public void simulateOneDay(int numBirds) {
 
         if(Math.random() < 0.05) currentFood = 0;
@@ -32,14 +25,20 @@ public class Feeder {
         }
 
     }
-    /**
-     * Returns the number of days birds or a bear found food to eat at the
-     * feeder in this simulation, as described in part (b) Preconditions:
-     * numBirds > 0, numDays > 0
-     */
-    public int simulateManyDays(int numBirds, int numDays) {
-        return 0;
+
+    public int simulateManyDays(int numBirds, int numDays)
+    {
+        int days = 0;
+        while(numDays > 0)
+        {
+            if(currentFood == 0) return days;
+            simulateOneDay(numBirds);
+            numDays --;
+            days ++;
+        }
+        return days;
     }
+
 
 
     public int getCurrentFood()
--- Main.java
+++ Main.java
@@ -3,8 +3,9 @@ public class Main {
         //System.out.println(f.currentFood);
         //System.out.println(g.getCurrentFood());
         Feeder i = new Feeder(1000);
-        i.simulateOneDay(5);
-        System.out.println();
-        System.out.println(i.toString(5));
+        System.out.println(i);
+        //i.simulateOneDay(5);
+        System.out.println(i.simulateManyDays(10,20));
+        System.out.println(i);
     }
 }
